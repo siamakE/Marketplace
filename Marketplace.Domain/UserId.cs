238ba82fc3c4386b709a6d29edae9edff56be293
Siamak Esmaeili <siamak@abacus.co>
2023-01-04 17:30:36 +0330

    Add project files.

---
+++ Marketplace.Domain/UserId.cs
@@ -0,0 +1,13 @@
+﻿namespace Marketplace.Domain;
+
+public class UserId
+{
+    private readonly Guid _value;
+    public UserId(Guid value)
+    {
+        if (value == default)
+            throw new ArgumentNullException(
+            nameof(value), "User id cannot be empty");
+        _value = value;
+    }
+}
